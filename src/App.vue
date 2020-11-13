1b6b09b97ed65467c3973d37625a2b3551b03ba6
Ndumiso Tshabalala <tshabalalandumiso3@gmail.com>
2020-11-13 14:41:51 +0200

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <Signup />
+  </div>
+</template>
+
+<script>
+
+import Signup from './components/Signup.vue';
+
+export default {
+    name: 'App',
+    components: {
+      Signup
+    }
+
+}
+</script>
+
