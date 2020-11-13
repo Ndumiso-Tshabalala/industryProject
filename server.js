877c9292cdc153b0b6abfde43ce1713fdff03de4
Ndumiso Tshabalala <tshabalalandumiso3@gmail.com>
2020-11-13 14:40:34 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// routes
+//sending data to the back end to sign up the user
+ app.post('/signup', (req,res,next)=> {
+    console.log(req.body)
+ })
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, (err) => {
+    if (err) return console.log(err);
+    console.log('server running on port ' + port);
+})
