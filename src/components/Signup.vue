cf23f49ae5d42faf5c5b47aab8c1f1ea14505c25
Ndumiso Tshabalala <tshabalalandumiso3@gmail.com>
2020-11-13 14:41:03 +0200

    Create Signup.vue
    First component

---
+++ src/components/Signup.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      NAME<input type="text" v-model="name"/><br/>
+      EMAIL<input type="text"  v-model="email"/><br/>
+      PASSWORD<input type="password"  v-model="password"/><br/>
+      <button @click="signup"> signup</button>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'Signup',
+    data(){
+        return{
+            name:'',
+            email:'',
+            password:''
+        }
+        },
+        methods: {
+            //calling data after sign up function is called
+            signup() {
+                       let newUser = {
+                       name: this.name,
+                       email: this.email,
+                       password: this.password
+               
+                }
+                // sending signUp data to backEnd
+                axios.post('http://localhost:5000/signup', newUser)
+               
+            }
+        }
+    }
+
+</script>
+
