3aaf153b9c33e2f673f3e0e4c2de10cf200104a7
Ndumiso Tshabalala <tshabalalandumiso3@gmail.com>
2020-11-14 01:09:31 +0200

    update

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 const cors = require('cors');
+const User = require('./models/User');
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -12,7 +13,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 // routes
 //sending data to the back end to sign up the user
  app.post('/signup', (req,res,next)=> {
-    console.log(req.body)
+    //  console.log(req.body.email)
+         const newUser = new User({
+         email: req.body.email,
+         name: req.body.name,
+         password: bcrypt.hashSync(req.body.password,10)
+         
+
+     })
+     console.log(newUser);
  })
 
 const port = process.env.PORT || 5000;
