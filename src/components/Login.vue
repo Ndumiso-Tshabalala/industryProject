3e48c5c2f5c68d0e1604c898a0a021343ba0652d
Ndumiso Tshabalala <tshabalalandumiso3@gmail.com>
2020-11-14 18:02:20 +0200

    Update Login.vue
    authentication

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,6 +3,7 @@
       EMAIL<input type="text"  v-model="email"/><br/>
       PASSWORD<input type="password"  v-model="password"/><br/>
       <button @click="login"> login</button>
+      {{ error }}
 
   </div>
 </template>
@@ -24,13 +25,18 @@ export default {
                 password: this.password
             }
              axios.post('http://localhost:5000/login', user)
-              .then(res =>{
-                    console.log(res)
-                })
-        
-        
-        
-  
+              .then(res => {
+                  //if log in successful
+                  if(res.status == 200){
+                      localStorage.setItem('token', res.data.token); //to see if user is authenticated or not
+                      this.$router.push('/'); //user is authorized and pushed to landing page
+                  }
+                  
+              }, err => {
+                  console.log(err.response);
+                  this.error = err.response.data.error
+
+              })
         }
     }
 
