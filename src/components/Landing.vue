a818550171363b595376a97ffde5284fbb1af090
Ndumiso Tshabalala <tshabalalandumiso3@gmail.com>
2020-11-14 18:02:40 +0200

    Update Landing.vue
    to see if user is authenticated(whether token exists within the local storage)

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -7,7 +7,13 @@ landing
 
 <script>
 export default {
-    name: 'Landing'
+    name: 'Landing', // to see if user is authenticated(whether token exists within the local storage)
+    created(){
+        //user not authorized
+        if(localStorage.getItem('token') === null) {
+         this.$router.push('/login');   //push back to log in page
+        }
+    }
 }
 </script>
 
